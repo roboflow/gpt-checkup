a7bb1c9aace8a5caf8870ee82e3904437e4f0af7
Leo Ueno <leo@roboflow.com>
2023-11-17 07:33:17 -0800

    Indentation fixes

--- web.py
+++ web.py
@@ -232,31 +232,31 @@ def object_detection():
     return iou, inference_time, str(answer)
 
 def set_of_mark():
-  base_model = GPT4V(
-      ontology=CaptionOntology({"none": "none"}),
-      api_key=os.environ["OPENAI_API_KEY"],
-  )
+    base_model = GPT4V(
+        ontology=CaptionOntology({"none": "none"}),
+        api_key=os.environ["OPENAI_API_KEY"],
+    )
 
-  result, inference_time = base_model.predict(
-      "images/fruits_som.png",
-      classes=[],
-      result_serialization="text",
-      prompt="Find all the fruits in this image and return a JSON array of all the applicable numbers.",
-  )
+    result, inference_time = base_model.predict(
+        "images/fruits_som.png",
+        classes=[],
+        result_serialization="text",
+        prompt="Find all the fruits in this image and return a JSON array of all the applicable numbers.",
+    )
 
-  code_regex = r'```[a-zA-Z]*\n(.*?)\n```'
-  code_blocks = re.findall(code_regex,result, re.DOTALL)
-  answer = json.loads(code_blocks[0])
+    code_regex = r'```[a-zA-Z]*\n(.*?)\n```'
+    code_blocks = re.findall(code_regex, result, re.DOTALL)
+    answer = json.loads(code_blocks[0])
 
-  correct = [35,40,26,2,13,17,29,21,10,42,8,43,0,11,7,4,12,27,37,39,22,15,25]
+    correct = [35,40,26,2,13,17,29,21,10,42,8,43,0,11,7,4,12,27,37,39,22,15,25]
 
-  score = 0
-  for guess in answer:
-      if guess in correct: score += 1
+    score = 0
+    for guess in answer:
+        if guess in correct: score += 1
 
-  accuracy = score/len(correct)
+    accuracy = score/len(correct)
 
-  return accuracy, inference_time, result
+    return accuracy, inference_time, result
 
 
 tests = [
