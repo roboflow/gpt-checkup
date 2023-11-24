4e1d5a9b7d909f4df525404d4f41afeb7aaa9c80
Jessica <jchens@stanford.edu>
2023-11-24 01:20:05 -0500

    Open passing tests by default

--- index.js
+++ index.js
@@ -3,35 +3,23 @@ let i;
 for (i = 0; i < dropdowns.length; i++) {
     dropdowns[i].addEventListener("click", function (event) {
         this.classList.toggle("active");
-
         let content = this.nextElementSibling;
-        // if (this.classList.contains("dropdown_learn")) {
-        //     content = this.nextElementSibling;
-        // } else {
-        //     content = this.previousElementSibling;
-        // }
-        // console.log(this.classList);
 
         console.log("before: " + content.style.display);
-        // flex, null
-        // none, content.style.display
-        if (content.style.display === "none") {
-            content.style.display = "flex";
+        if (content.style.padding !== "0px var(--spacing-sizing-6") {
+            content.style.padding = "0px var(--spacing-sizing-6";
         } else {
-            content.style.display = "none";
+            content.style.padding = "0 var(--spacing-sizing-6) var(--spacing-sizing-4) var(--spacing-sizing-6)";
         }
         console.log("after: " + content.style.display);
 
         if (content.style.maxHeight != "0px") {
             content.style.maxHeight = "0px";
-            // content.style.padding = "0px";
         } else {
             content.style.maxHeight = content.scrollHeight + "px";
-            // content.style.padding = "0 var(--spacing-sizing-6) var(--spacing-sizing-4) var(--spacing-sizing-6)";
         }
     });
 }
-
 let show_passing_btn = document.querySelector("#show_passing_btn");
 show_passing_btn.addEventListener("click", function (event) {
     this.classList.toggle("show_passing_btn_active");
@@ -42,14 +30,14 @@ show_passing_btn.addEventListener("click", function (event) {
     }
 
     let content = document.querySelector("#passing_cards");
-    if (content.style.overflow === "visible") {
+    if (content.style.overflow === "visible" || !content.style.overflow) {
         content.style.overflow = "hidden";
     } else {
         content.style.overflow = "visible";
     }
-    if (content.style.maxHeight) {
-        content.style.maxHeight = null;
+    if (content.style.maxHeight != "0px" || !content.style.maxHeight) {
+        content.style.maxHeight = "0px";
     } else {
-        content.style.maxHeight = "1000vh";
+        content.style.maxHeight = content.scrollHeight + "px";
     }
 });
