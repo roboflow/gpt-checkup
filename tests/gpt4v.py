928b1aef0a93af895d2fd7f76d04baeb73049d81
James <37276661+capjamesg@users.noreply.github.com>
2025-02-12 18:35:42 +0000

    Update gpt4v.py

--- tests/gpt4v.py
+++ tests/gpt4v.py
@@ -47,7 +47,7 @@ class GPT4V(DetectionBaseModel):
         start_time = time.time()
 
         response = self.client.chat.completions.create(
-            model="o1-mini",
+            model="o1",
             messages=payload,
             max_completion_tokens=300,
         )
