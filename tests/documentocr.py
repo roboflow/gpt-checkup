afe2c0ea1ba710f41558c9f200190c8686d61778
James <37276661+capjamesg@users.noreply.github.com>
2025-02-18 09:30:35 +0000

    Update documentocr.py

--- tests/documentocr.py
+++ tests/documentocr.py
@@ -8,7 +8,7 @@ import re
 class DocumentOCRTest:
     name = "Document OCR"
     id = "document_ocr"
-    question = "Can GPT-4V read a document and return the exact characters in the text?"
+    question = "Can GPT read a document and return the exact characters in the text?"
     prompt = "Read the text in the image. Return only the text, with punctuation."
     image = "images/swift.png"
     method = "We send a screenshot of a typed document to determine if it can correctly read the text. If it correctly gets the text, it gets a 100%. Otherwise, it gets a 0%."
